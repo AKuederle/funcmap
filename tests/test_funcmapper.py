1eccce52365d7a249e19d951c615b3c45cb9b835
Arne Kuederle <a.kuederle@gmail.com>
2018-01-10 09:13:18 +0100

    Added tests for new functional mapping. Refactored tests

--- tests/test_funcmapper.py
+++ tests/test_funcmapper.py
@@ -3,6 +3,7 @@
 import unittest
 
 import re
+
 try:
     from mock import MagicMock
 except ImportError:
@@ -12,41 +13,68 @@ from funcmap import FuncMapper
 from funcmap.mapper import NotSupportedError
 
 
-class TestMapping(unittest.TestCase):
-    """All tests for mapping a function."""
-
-    def setUp(self):
-        """Set up."""
-        self.mapper = FuncMapper()
+class BaseMappingTest:
+    def _mapper_func(self, regex, func):
+        NotImplementedError('Implement in Child class')
 
     def test_simple_map(self):
         """Test that simple mapping of a function works."""
+
         def test_func(): pass
-        self.mapper.map('test')(test_func)
+
+        self._mapper_func('test', test_func)
         self.assertEqual(self.mapper._mapped_functions['test'], test_func)
         self.assertEqual(self.mapper._mapped_regex['test'], re.compile('test'))
 
     def test_kwargs_map(self):
         """Test that mapping with kwargs works."""
+
         def test_func(kwarg1, kwarg2): pass
+
         regex = r'test (?P<kwarg1>.+) (?P<kwarg2>.+)'
-        self.mapper.map(regex)(test_func)
+        self._mapper_func(regex, test_func)
         self.assertEqual(self.mapper._mapped_functions[regex], test_func)
         self.assertEqual(self.mapper._mapped_regex[regex], re.compile(regex))
 
     def test_args_map_failed(self):
         """Test that mapping with unnamed capture groups fails."""
+
         def test_func(arg1, arg2): pass
+
         regex = r'test (.+) (.+)'
         with self.assertRaises(NotSupportedError):
-            self.mapper.map(regex)(test_func)
+            self._mapper_func(regex, test_func)
 
     def test_args_and_kwargs_map_failed(self):
         """Test that mapping with unnamed capture groups and named captured groups fails."""
+
         def test_func(arg1, arg2): pass
+
         regex = r'test (?P<kwarg1>.+) (.+)'
         with self.assertRaises(NotSupportedError):
-            self.mapper.map(regex)(test_func)
+            self._mapper_func(regex, test_func)
+
+
+class TestFunctionalMapping(unittest.TestCase, BaseMappingTest):
+    """All tests for mapping a function using the functional syntax."""
+
+    def _mapper_func(self, regex, func):
+        self.mapper.map(regex, func)
+
+    def setUp(self):
+        """Set up."""
+        self.mapper = FuncMapper()
+
+
+class TestDecoratorMapping(unittest.TestCase, BaseMappingTest):
+    """All tests for mapping a function using the decorator syntax."""
+
+    def _mapper_func(self, regex, func):
+        self.mapper.map(regex)(func)
+
+    def setUp(self):
+        """Set up."""
+        self.mapper = FuncMapper()
 
 
 class TestRetrieve(unittest.TestCase):
@@ -89,5 +117,3 @@ class TestRetrieve(unittest.TestCase):
         """Test that an exception is raised if no match is found."""
         with self.assertRaises(KeyError):
             self.mapper('test1')
-
-
